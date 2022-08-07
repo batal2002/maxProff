1a71f2bee6c0264ae99260b1e556cbc351d4a8fb
batal2002 <64532777+batal2002@users.noreply.github.com>
2022-08-07 11:58:52 +0300

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,228 @@
+<?php
+Header("HTTP/1.1 404 Not Found");
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/MP.svg">
+    <link rel="stylesheet" href="css/404.min.css?_v=20220802224235">
+    <title>404</title>
+</head>
+    <body style="height: 100vh; display: flex; flex-direction: column;">
+        
+        <div class="overlay"></div>
+        <header class="header">
+    <div class="container header__container">
+        <div class="header__top">
+            <div class="header__burger">
+                <span></span>
+            </div>
+            <a href="index.html" class="logo header__logo">Max<span class="--green">P</span>roff</a>
+            <a href="tel:+72332222222" class="header__button-mobile"></a>
+            <nav class="header__nav">
+                <a href="index.html#price" class="header__anchor">Цены</a>
+                <a href="index.html#" class="header__anchor">Услуги</a>
+                <a href="index.html#works" class="header__anchor">Портфолио</a>
+                <a href="index.html#stocks" class="header__anchor">Акции</a>
+                <a href="index.html#benefit" class="header__anchor">Преимущества</a>
+                <a href="index.html#channel" class="header__anchor">Блог</a>
+                <a href="index.html#" class="header__anchor">Контакты</a>
+            </nav>
+            <a href="tel:+72332222222" class="tel header__tel">
+                <div class="tel__header">
+                    <img src="img/icon/tel.svg" alt="">
+                    <span class="tel__number">+7 (233) 222-22-22</span>
+                </div>
+                <span class="tel__footer">08:00 - 22:00, без выходных</span>
+            </a>
+            <button class="open-modal header__button">Заказать звонок</button>
+        </div>
+        <nav class="header__menu">
+            <button class="open-modal header__button">Заказать звонок</button>
+            <div class="header__wrapper">
+                <span class="header__link">Ремонт квартир</span>
+                <div class="header__dropdown">
+                    <ul class="header__list">
+                        <li><a href="index.html#" class="header__dropdown-link">Черновой</a></li>
+                        <li><a href="index.html#" class="header__dropdown-link">Косметический</a></li>
+                        <li><a href="index.html#" class="header__dropdown-link">Капитальный</a></li>
+                        <li><a href="index.html#" class="header__dropdown-link">Элитный</a></li>
+                        <li><a href="index.html#" class="header__dropdown-link">В новостройках</a></li>
+                        <li><a href="index.html#" class="header__dropdown-link">Студии</a></li>
+                        <li><a href="index.html#" class="header__dropdown-link">Вторичное жилье</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="header__wrapper">
+                <span class="header__link">Дизайн интерьера</span>
+                <div class="header__dropdown">
+                    <ul class="header__list">
+                        <li><a href="index.html#" class="header__dropdown-link">Дизайн кухни</a></li>
+                        <li><a href="index.html#" class="header__dropdown-link">Дизайн спальни</a></li>
+                        <li><a href="index.html#" class="header__dropdown-link">Дизайн гостинной</a></li>
+                        <li><a href="index.html#" class="header__dropdown-link">Дизайн детской</a></li>
+                        <li><a href="index.html#" class="header__dropdown-link">Дизайн санузла</a></li>
+                        <li><a href="index.html#" class="header__dropdown-link">Дизайн коридора</a></li>
+                        <li><a href="index.html#" class="header__dropdown-link">Дизайн балкона</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="header__wrapper">
+                <span class="header__link">Ремонт комнат</span>
+                <div class="header__dropdown">
+                    <ul class="header__list">
+                        <li><a href="index.html#" class="header__dropdown-link">Кухни</a></li>
+                        <li><a href="index.html#" class="header__dropdown-link">Спальни</a></li>
+                        <li><a href="index.html#" class="header__dropdown-link">Гостинная</a></li>
+                        <li><a href="index.html#" class="header__dropdown-link">Детские</a></li>
+                        <li><a href="index.html#" class="header__dropdown-link">Санузлы</a></li>
+                        <li><a href="index.html#" class="header__dropdown-link">Коридоры</a></li>
+                        <li><a href="index.html#" class="header__dropdown-link">Балконы</a></li>
+                    </ul>
+                </div>
+            </div>
+            <a href="index.html#" class="header__link">Дома и коттеджи</a>
+            <a href="index.html#" class="header__link">Прочие услуги</a>
+
+            <nav class="header__nav">
+                <a href="index.html#price" class="header__anchor">Цены</a>
+                <a href="index.html#" class="header__anchor">Услуги</a>
+                <a href="index.html#" class="header__anchor">Портфолио</a>
+                <a href="index.html#" class="header__anchor">Акции</a>
+                <a href="index.html#" class="header__anchor">Преимущества</a>
+                <a href="index.html#" class="header__anchor">Блог</a>
+                <a href="index.html#" class="header__anchor">Контакты</a>
+            </nav>
+
+            <a href="tel:+72332222222" class="tel header__tel">
+                <div class="tel__header">
+                    <img src="img/icon/tel.svg" alt="">
+                    <span class="tel__number">+7 (233) 222-22-22</span>
+                </div>
+                <span class="tel__footer">08:00 - 22:00, без выходных</span>
+            </a>
+        </nav>
+    </div>
+</header>
+        <main>
+            <section class="error">
+    <div class="container error__container">
+        <h2 class="error__title">Страница не найдена</h2>
+        <a href="index.html" class="error__text">Перейдите на главную страницу</a>
+    </div>
+</section>
+        </main>
+        <footer class="footer">
+    <div class="container footer__container">
+        <div class="footer__top">
+            <div class="footer__column">
+                <a href="#" class="logo footer__logo"><span class="--green">M</span>ax<span class="--green">P</span>roff</a>
+                <span class="footer__pay-text">Любая форма оплаты</span>
+                <div class="footer__pay">
+                    <div class="footer__pay-icon">
+                        <img src="img/footer/wallet.svg" alt="">
+                    </div>
+                    <div class="footer__pay-icon">
+                        <img src="img/footer/sber.svg" alt="">
+                    </div>
+                    <div class="footer__pay-icon">
+                        <picture><source srcset="img/footer/visa.webp" type="image/webp"><img src="img/footer/visa.png" alt=""></picture>
+                    </div>
+                    <div class="footer__pay-icon">
+                        <picture><source srcset="img/footer/alfa.webp" type="image/webp"><img src="img/footer/alfa.png" alt=""></picture>
+                    </div>
+                    <div class="footer__pay-icon">
+                        <picture><source srcset="img/footer/mastercard.webp" type="image/webp"><img src="img/footer/mastercard.png" alt=""></picture>
+                    </div>
+                </div>
+            </div>
+            <div class="footer__column">
+                <h4 class="footer__subtitle">Наши услуги</h4>
+                <ul>
+                    <li class="footer__link"><a href="#">Квартиры</a></li>
+                    <li class="footer__link"><a href="#">Коттеджи</a></li>
+                    <li class="footer__link"><a href="#">Таунхаусы</a></li>
+                    <li class="footer__link"><a href="#">Офисы</a></li>
+                    <li class="footer__link"><a href="#">Инженерные системы</a></li>
+                    <li class="footer__link"><a href="#">География работ</a></li>
+                </ul>
+            </div>
+            <div class="footer__column">
+                <ul class="footer__list">
+                    <li><a href="#works" class="footer__anchor">Портфолио</a></li>
+                    <li><a href="#price" class="footer__anchor">Стоимость</a></li>
+                    <li><a href="#stocks" class="footer__anchor">Акции</a></li>
+                    <li><a href="#how" class="footer__anchor">О компании</a></li>
+                    <li><a href="#channel" class="footer__anchor">Статьи</a></li>
+                    <li><a href="#" class="footer__anchor">Контакты</a></li>
+                </ul>
+            </div>
+            <div class="footer__column">
+                <div class="footer__tel">
+                    <div class="footer__tel-img">
+                        <img src="img/footer/phone.svg" alt="">
+                    </div>
+                    <div>
+                        <a href="tel:+72332222222" class="footer__tel-number">+7 (233) 222-22-22</a>
+                        <span class="footer__tel-time">08:00 - 22:00, без выходных</span>
+                        <span class="footer__tel-order open-modal">Заказать звонок</span>
+                    </div>
+                </div>
+                <a href="mailto:@" class="footer__mail">
+                    <img src="img/icon/mail.svg" alt="">
+                    <span>Электронная почта @</span>
+                </a>
+                <a href="" class="footer__address">
+                    <img src="img/icon/map-pin-light.svg" alt="">
+                    <span>Адрес</span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="container footer__container">
+        <div class="footer__bot">
+            <span class="footer__copyright"><span>©</span> 2021 «MaxProff» <a href="#">Правовая информация</a></span>
+            <div class="footer__media">
+                <a href="https://www.facebook.com" class="footer__media-icon">
+                    <img src="img/footer/facebook.svg" alt="">
+                </a>
+                <a href="https://www.youtube.com" class="footer__media-icon">
+                    <img src="img/footer/youtube.svg" alt="">
+                </a>
+                <a href="https://www.instagram.com" class="footer__media-icon">
+                    <img src="img/footer/inst.svg" alt="">
+                </a>
+                <a href="https://desktop.telegram.org" class="footer__media-icon">
+                    <img src="img/footer/tg.svg" alt="">
+                </a>
+                <a href="https://vk.com" class="footer__media-icon">
+                    <img src="img/footer/vk.svg" alt="">
+                </a>
+                <a href="https://ok.ru" class="footer__media-icon">
+                    <picture><source srcset="img/footer/odnoklassniki.webp" type="image/webp"><img src="img/footer/odnoklassniki.png" alt=""></picture>
+                </a>
+            </div>
+            <span class="footer__text">Сайт не является публичной офертой и носит информационный характер</span>
+            <a href="" class="footer__text-link">Карта сайта</a>
+        </div>
+    </div>
+</footer>
+        
+        <div class="modal">
+    <div class="modal__block">
+        <div class="modal__img">
+            <img loading="lazy" src="img/modal-done.svg" alt="">
+            <div class="modal__ellipse"></div>
+            <div class="modal__ellipse-blur"></div>
+        </div>
+        <span class="modal__title">Ваша заявка принята</span>
+        <p class="modal__text">В ближайшее время наш менеджер свяжется  с Вами</p>
+    </div>
+</div>
+        <script src="js/404.min.js?_v=20220802224235"></script>
+    </body>
+</html>
